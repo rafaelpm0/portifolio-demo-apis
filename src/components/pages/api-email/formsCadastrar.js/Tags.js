815d638d3c3ccbd13eaf462d3578ceb095fe77f6
Rafael Pinho Medeiros <rafaelpmedeiros00@gmail.com>
2024-06-10 09:06:40 -0300

    teste ajustando icons

--- src/components/pages/api-email/formsCadastrar.js/Tags.js
+++ src/components/pages/api-email/formsCadastrar.js/Tags.js
@@ -3,7 +3,10 @@ import { useEffect, useState } from "react";
 import conf from '../../../../conf'
 import Loading from "../../../layout/Loading";
 import Message from "../../../layout/Message";
-
+import { MdModeEdit, } from "react-icons/md";
+import { FaTrashAlt } from "react-icons/fa";
+import { FaCheckCircle } from "react-icons/fa";
+import { IoIosRemoveCircle } from "react-icons/io";
 
 function Tags() {
 
@@ -11,7 +14,7 @@ function Tags() {
     const [tagRetorno, setTagRetorno] = useState(true);
     const [isLoading, setIsLoading] = useState(false);
     const [message, setMessage] = useState('');
-    const [type, setType] = useState("");
+    const [type, setType] = useState();
     const [tag, setTag] = useState({});
     const [nameTags, setNameTags] = useState([]);
     const [contentTag, setContentTag] = useState([]);
@@ -19,19 +22,15 @@ function Tags() {
     const [alterTag, setAlterTag] = useState({});
 
     function handleInExOnChange(e) {
-        /*
-        setIncluirExcluir(e.target.value === 'true');*/
+        setIncluirExcluir(e.target.value === 'true');
     }
 
     function handleTagRetOnChange(e) {
-        /*
         setTagRetorno(e.target.value === 'true');
         setTag({});
-        */
     }
 
     function handleTaInsertgOnChange(e) {
-        /*
         let { name, value } = e.target;
 
 
@@ -45,22 +44,20 @@ function Tags() {
                 [name]: value
             }));
         }
-        */
+
     }
 
     function resetMessage() {
-        /*
         const timer = setTimeout(() => {
             setMessage("");
         }, 2900);
         return () => clearTimeout(timer);
-        */
     }
 
     function handleIncluirTag() {
-        /*
-        if (((Object.keys(tag).length !== 2) && (tagRetorno === false))   //Regra para tag, um campo e não pode ser repetido. Regra para retorno  2 campos apenas
-       
+
+        if (((Object.keys(tag).length !== 2) && (tagRetorno === false))   /*Regra para tag, um campo e não pode ser repetido. Regra para retorno
+        2 campos apenas*/
         || ((Object.keys(tag).length !== 1) && (tagRetorno === true))) {
             setType("error");
             setMessage("Preencha todos os campos!");
@@ -107,11 +104,9 @@ function Tags() {
                 resetMessage();
 
             });
-            */
     }
 
-    function atualizarNametags(data) {
-        /*
+    function atualizarNametags(data){
                 let list = [];
                 data.forEach((item) => {
                     if (!list.includes(item.nome)) {
@@ -119,10 +114,8 @@ function Tags() {
                     }
                   });
                 setNameTags(list);
-                */
     }
 
-    /*
     useEffect(() => {
 
         const fetchTag = async () => {
@@ -144,34 +137,27 @@ function Tags() {
         }
         fetchTag();
 
-    }, [incluirExcluir, tagRetorno]);*/
+    }, [incluirExcluir, tagRetorno]);
     function handleCurrentTagChange(e) {
-        /*
         setCurrentTag(e.target.value)
-        */
     };
 
     function handleEditeOnClick(item) {
-        /*
         if (item.referencia === alterTag.referencia) {
             setAlterTag("")
             return;
         }
         setAlterTag(item);
-        */
     }
 
     function handleEditeOnChange(e) {
-        /*
         setAlterTag((prevData) => ({
             ...prevData,
             retorno: e.target.value
         }))
-        */
     }
 
     function patchTag() {
-        /*
         setMessage("");
         setType("");
         const pTag = async () => {
@@ -209,10 +195,9 @@ function Tags() {
 
         }
         pTag();
-        */
     }
 
-    function handleDeleteOnClick(tag) { /*
+    function handleDeleteOnClick(tag) {
         setMessage("");
         setType("");
 
@@ -264,11 +249,176 @@ function Tags() {
         }
         handleDelete();
 
-        */
+
     }
     return (
-       <p></p>
-        );
+        <div className={styles.tags}>
+
+            <div className={styles.incluirExcluir}>
+
+                <div className={styles.elementInEx}>
+
+                    <label htmlFor="incluir"> Incluir </label>
+                    <input type="checkbox" id="incluir" value={true}
+                        checked={incluirExcluir} onChange={handleInExOnChange} />
+
+                    <label htmlFor="incluir">
+                        <FaCheckCircle style={incluirExcluir ? { color: '#FFBB33' } : ""} />
+                    </label>
+
+                </div>
+                <div className={styles.elementInEx}>
+
+                    <label htmlFor="excluir">Alterar/Excluir </label>
+                    <input type="checkbox" id="excluir" value={false}
+                        checked={!incluirExcluir} onChange={handleInExOnChange} />
+
+                    <label htmlFor="excluir">
+                        <FaCheckCircle style={!incluirExcluir ? { color: '#FFBB33' } : ""} />
+                    </label>
+
+                </div>
+
+
+            </div>
+            {incluirExcluir && (
+
+                <div>
+                    <div class={styles.tagRetorno}>
+                        <div className={styles.elementInEx}>
+
+                            <label htmlFor="tag"> Tag </label>
+                            <input type="checkbox" id="tag" value={true}
+                                checked={tagRetorno} onChange={handleTagRetOnChange} />
+
+                            <label htmlFor="tag">
+                                <FaCheckCircle style={tagRetorno ? { color: '#FFBB33' } : ""} />
+                            </label>
+
+                        </div>
+                        <div className={styles.elementInEx}>
+
+                            <label htmlFor="retorno"> Retorno </label>
+                            <input type="checkbox" id="retorno" value={false}
+                                checked={!tagRetorno} onChange={handleTagRetOnChange} />
+
+                            <label htmlFor="retorno">
+                                <FaCheckCircle style={!tagRetorno ? { color: '#FFBB33' } : ""} />
+                            </label>
+
+                        </div>
+                    </div>
+
+                    <div className={styles.alinhamentoEntradas}>
+                        <p className={styles.entradas}>Informe: </p>
+
+
+                        {tagRetorno && (
+                            <div className={styles.entradas}>
+                                <label htmlFor="tag">Tag: </label>
+                                <input type="text" name="nome" onChange={handleTaInsertgOnChange} />
+                            </div>
+                        )}
+
+
+                        {!tagRetorno && (
+                            <div>
+                                <div className={styles.entradas}>
+                                    <label htmlFor="">Nome: </label>
+                                    <select name="nome" id="nome" onChange={handleTaInsertgOnChange}>
+                                        <option key={"nome"} value={""}>Selecione uma tag</option>
+                                        {
+                                            nameTags.map((item) => (
+                                                <option key={item} value={item}>{item}</option>
+                                            ))
+                                        }
+                                    </select>
+                                </div>
+                                <div className={styles.entradas}>
+                                    <label htmlFor="">Retorno: </label>
+                                    <input type="text" name="retorno" onChange={handleTaInsertgOnChange} />
+                                </div>
+                            </div>
+                        )}
+
+                        <div className={styles.button}>
+                            <button onClick={handleIncluirTag}>Enviar</button>
+                        </div>
+                    </div>
+                </div>
+
+            )}
+
+            {!incluirExcluir && (
+                <div>
+                    <div className={styles.entradas}>
+                        <label htmlFor="">Selecione a tag: </label>
+                        <select name="tag" id="tag" onChange={handleCurrentTagChange}>
+                            <option key={""} value={""}>Selecione uma tag</option>
+                            {
+                                nameTags.map((item) => (
+                                    <option key={item} value={item}>{item}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    <div>
+                        {
+                            contentTag.map((item) =>
+                                (item.nome === currentTag) && (
+                                    <div className={`${styles.entradas} ${styles.b} ${styles.c}`}>
+                                        <label htmlFor="alterTag">{item.nome} : </label>
+
+                                        {(alterTag.referencia === item.referencia) ?
+                                            (
+                                                <input name={item.nome} type="text"
+                                                    value={alterTag.retorno} onChange={handleEditeOnChange} />
+
+                                            )
+                                            : (<p>{item.retorno}</p>)
+                                        }
+
+                                        <div className={styles.editarExcluir}>
+                                            <button onClick={() => handleEditeOnClick(item)}>
+                                                <MdModeEdit size={20} />
+                                            </button>
+                                            <button onClick={() => handleDeleteOnClick(item)}>
+                                                <FaTrashAlt 
+                                                    size={16} />
+
+                                            </button>
+                                        </div>
+
+
+                                        {((alterTag.referencia === item.referencia) && (alterTag.retorno !== item.retorno)) &&
+                                            (<div className={styles.editarExcluir}>
+                                                <button onClick={patchTag}><FaCheckCircle size={17} /></button>
+                                                <button onClick={() => handleEditeOnClick(item)}><IoIosRemoveCircle size={20} /></button>
+                                            </div>)
+
+                                        }
+
+
+                                    </div>)
+                            )
+                        }
+                    </div>
+
+                </div>
+            )}
+
+            <div className={styles.message}>
+                {
+                    isLoading && (
+                        <Loading />
+                    )}
+            </div>
+            <div className={styles.message}>
+                <Message msg={message} type={type} reset={resetMessage} />
+            </div>
+
+        </div>
+    );
 }
 
 export default Tags;
