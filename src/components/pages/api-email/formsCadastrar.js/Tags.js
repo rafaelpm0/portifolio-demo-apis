b7edab0951b8630d40acbd22cff6e91d65caf0a7
Rafael Pinho Medeiros <rafaelpmedeiros00@gmail.com>
2024-06-10 10:31:19 -0300

    trocando md

--- src/components/pages/api-email/formsCadastrar.js/Tags.js
+++ src/components/pages/api-email/formsCadastrar.js/Tags.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import conf from '../../../../conf'
 import Loading from "../../../layout/Loading";
 import Message from "../../../layout/Message";
+import { RiPencilFill } from "react-icons/ri";
 import { FaTrashAlt } from "react-icons/fa";
 import { FaCheckCircle } from "react-icons/fa";
 
@@ -378,7 +379,7 @@ function Tags() {
 
                                         <div className={styles.editarExcluir}>
                                             <button onClick={() => handleEditeOnClick(item)}>
-                                                <FaTrashAlt size={20} />
+                                                <RiPencilFill  size={20} />
                                             </button>
                                             <button onClick={() => handleDeleteOnClick(item)}>
                                                 <FaTrashAlt 
